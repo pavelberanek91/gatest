65c04c80a536ba813891082f73cbc3fa58ec70fc
pavelberanek91 <pavelberanek91@gmail.com>
2024-04-19 15:33:56 +0200

    bla

--- test_app.py
+++ test_app.py
@@ -1,4 +1,15 @@
-from app import add
+from app import add, sub
 
-assert add(2, 3) == 5
 
+def test_add_two_positive():
+    a = 2
+    b = 3
+    result = add(a, b)
+    assert result == 5
+
+
+def test_sub_two_positive():
+    a = 2
+    b = 3
+    result = sub(a, b)
+    assert result == -1
